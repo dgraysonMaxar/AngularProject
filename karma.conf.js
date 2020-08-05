2ddac31590443ad89eb278bbafe6fd90bc8d78d9
David Grayson <david.grayson@maxar.com>
2020-08-05 15:18:07 -0400

    modify to use headless

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: ['--headless --no-sandbox']
+      }
+    },
+    singleRun: true
   });
 };
